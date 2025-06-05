6d9586185216067ae2d6e85f88dd9499ea522966
Ansh-Tope <topeansh928@gmail.com>
2025-06-05 14:06:04 +0530

    all

--- js/script.js
+++ js/script.js
@@ -22,37 +22,18 @@ async function main() {
     let songs = await getSongs()
     console.log(songs);
 
-    let songUl=document.querySelector(".song-list").getElementsByTagName("ul")[0]
+    let songUl = document.querySelector(".song-list").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUl.innerHTML=songUl.innerHTML+`<li>
-        
-    
-                            <img src="/img/music.svg" alt="">
+        songUl.innerHTML = songUl.innerHTML + `<li><img src="/img/music.svg" alt="">
                             <div class="info">
-                                <div>${song.replaceAll("%20"," ")}</div>
+                                <div>${song.replaceAll("%20", " ")}</div>
                                 <div>Song Artist</div>
                             </div>
                             <div class="playnow">
                                 <span>Play Now</span>
                                 <img  src="/img/play button.svg" alt="">
-                            </div>
-
-                        
-        
-        
-        
-        
-        </li>`
+                            </div></li>`
     }
-
-    var audio = new Audio(songs[0]);
-    // audio.play();
-
-    audio.addEventListener("loadeddata",()=>{
-        let duration=audio.duration
-        console.log(duration);
-        
-    })
 
 }
 main()
