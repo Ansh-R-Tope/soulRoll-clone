2c26b42c4b484058718991828ffb6f4378056ecf
Ansh-Tope <topeansh928@gmail.com>
2025-07-02 20:51:59 +0530

    all

--- js/script.js
+++ js/script.js
@@ -21,46 +21,37 @@ function secondsToMinutes(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
-    let response = await a.text()
 
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let as = div.getElementsByTagName('a')
-    songs = []
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/${folder}/`)[1])
-        }
-    }
+    // fetch the song list from manifest.json
+    let response = await fetch(`${folder}/manifest.json`);
+    let data = await response.json();
 
-    let songUl = document.querySelector(".song-list").getElementsByTagName("ul")[0]
-    songUl.innerHTML = ""
+    songs = data.songs;
+
+    let songUl = document.querySelector(".song-list").getElementsByTagName("ul")[0];
+    songUl.innerHTML = "";
+
     for (const song of songs) {
-        songUl.innerHTML = songUl.innerHTML + `<li><img src="/img/music.svg" alt="">
-                            <div class="info">
-                                <div>${song.replaceAll("%20", " ")}</div>
-                                <div>Song Artist</div>
-                            </div>
-                            <div class="playnow">
-                                <span>Play Now</span>
-                                <img  src="/img/play button.svg" alt="">
-                            </div></li>`
+        songUl.innerHTML += `<li><img src="/img/music.svg" alt="">
+            <div class="info">
+                <div>${song}</div>
+                <div>Song Artist</div>
+            </div>
+            <div class="playnow">
+                <span>Play Now</span>
+                <img  src="/img/play button.svg" alt="">
+            </div></li>`;
     }
-
-    // applying event listener to every song
 
     Array.from(document.querySelector(".song-list").getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click", element => {
-            console.log(e.querySelector(".info").firstElementChild.innerHTML);
+        e.addEventListener("click", () => {
             playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
-        })
-    })
+        });
+    });
 
-    return songs
-
+    return songs;
 }
+
 
 const playMusic = (track, pause = false) => {
     // let audio = new Audio("/songs/" + track)
